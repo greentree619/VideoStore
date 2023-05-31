5aea4ee6fc8a9514a82bd619c9abc9b8cdef9a43
Green Full Stack <greentree619@outlook.com>
2023-05-31 07:28:56 +0200

    Patch for download image

---
+++ DownloadImageFromOpenAILink/lambda_function.py
@@ -0,0 +1,59 @@
+import json
+import boto3
+import requests
+from datetime import datetime
+import random
+import mimetypes
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    url = "https://i.stack.imgur.com/38FoP.jpg?s=64&g=1"  # Replace with the URL of the file you want to download
+    bucket_name = "article-image-bucket-live"  # Replace with your S3 bucket name
+    
+    # Download the file from the URL
+    response = requests.get(url)
+    if response.status_code == 200:
+        file_content = response.content
+        content_type = response.headers.get('Content-Type')
+        file_extension = mimetypes.guess_extension(content_type)
+        print("file_extension:", file_extension)
+        
+        key = get_date() +"/" + get_subkey() + f"/{generate_random_number()}{file_extension}"  # Replace with the desired key or filename in the S3 bucket
+        print(key)
+        
+        # Save the file to S3 bucket
+        s3.put_object(Body=file_content, Bucket=bucket_name, Key=key)
+        
+        return {
+            'statusCode': 200,
+            'body': 'File downloaded and saved to S3 bucket'
+        }
+    else:
+        return {
+            'statusCode': response.status_code,
+            'body': 'Failed to download file from URL'
+        }
+
+def get_date():
+    current_datetime = datetime.now()
+    datetime_string = current_datetime.strftime("%Y-%m-%d")
+    return datetime_string
+
+def get_subkey():
+    current_datetime = datetime.now()
+    milliseconds = current_datetime.strftime("%f")[:-3]  # Extract milliseconds and remove trailing zeros
+    datetime_string = current_datetime.strftime("%H%M%S") + f"{milliseconds}"
+    return datetime_string
+    
+def generate_random_number():
+    min_range = 1000000  # Minimum 7-digit number (inclusive)
+    max_range = 9999999  # Maximum 7-digit number (inclusive)
+    return random.randint(min_range, max_range)
+    
+# def lambda_handler(event, context):
+#     # TODO implement
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps('Hello from Lambda!')
+#     }
---
+++ Img-thumnail-generator/imageThumbGenerator/index.js
@@ -0,0 +1,58 @@
+const AWS = require('aws-sdk');
+const sharp = require('sharp');
+
+const s3 = new AWS.S3();
+var THUMBNAIL_MAX_WIDTH  = 300;
+var THUMBNAIL_MAX_HEIGHT = 300;
+
+exports.handler = async (event, context) => {
+  try {
+    //console.log("source bueckt info:", event.Records[0].s3);
+    console.log("source bueckt info:", event.Records[0].s3.bucket.name, event.Records[0].s3.object.key);
+    const sourceBucket = event.Records[0].s3.bucket.name;
+    const sourceKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    const destinationBucket = 'article-image-thumbnails-bucket-live';
+    const destinationKey = `${sourceKey}`;
+
+    // Fetch the image from the source S3 bucket
+    const params = {
+      Bucket: sourceBucket,
+      Key: sourceKey
+    };
+    const imageObject = await s3.getObject(params).promise();
+
+    // Generate the thumbnail using sharp library
+    const metadata = await await sharp(imageObject.Body).metadata();
+    var scalingFactor = Math.min(
+        THUMBNAIL_MAX_WIDTH/metadata.width,
+        THUMBNAIL_MAX_HEIGHT/metadata.height
+    );
+    var width  = parseInt(scalingFactor*metadata.width, 10);
+    var height = parseInt(scalingFactor*metadata.height, 10);
+    
+    const thumbnailBuffer = await sharp(imageObject.Body)
+      .resize(width, height)
+      .toBuffer();
+
+    // Save the thumbnail to the destination S3 bucket
+    const uploadParams = {
+      Bucket: destinationBucket,
+      Key: destinationKey,
+      Body: thumbnailBuffer
+    };
+    await s3.upload(uploadParams).promise();
+
+    console.log('Thumbnail generated and saved successfully');
+  } catch (error) {
+    console.error('Error generating thumbnail:', error);
+  }
+};
+
+// exports.handler = async (event) => {
+//     // TODO implement
+//     const response = {
+//         statusCode: 200,
+//         body: JSON.stringify('Hello from Lambda!'),
+//     };
+//     return response;
+// };
